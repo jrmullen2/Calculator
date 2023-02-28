e7738a6a5036955bc54ac0043ecc8c65bb73c70d
Eena <106263125+jrmullen2@users.noreply.github.com>
2023-02-28 05:42:54 -0800

    Create operate function

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function divide(...args)
 {
     return args.reduce((quotient, currentValue) => quotient /= currentValue);
 }
+function operate(op, num1, num2)
+{
+    switch(op)
+    {
+        case "+":
+            add(num1, num2)
+            break;
+        case "-":
+            subtract(num1, num2)
+            break;
+        case "*":
+            multiply(num1, num2)
+            break;
+        case "/":
+            divide(num1, num2)
+            break;
+    }
+}
