c51b71e6dcebc95549d4125435733657197808e3
Eena <106263125+jrmullen2@users.noreply.github.com>
2023-03-01 14:02:36 -0800

    Create for loop that makes divs with buttons. Create noNumSpots and assignOperator functions

--- script.js
+++ script.js
@@ -33,3 +33,49 @@ function operate(op, num1, num2)
             break;
     }
 }
+const container = document.querySelector("#container");
+for(let i = 0; i < 19; i++)
+{
+    const newDiv = document.createElement("div");
+    const newButton = document.createElement("button");
+    newDiv.appendChild(newButton);
+    container.appendChild(newDiv);
+    if(!(nonNumSpots(i)) && i < 11)
+    {
+        newButton.textContent = i.toString();
+    }
+    else if(nonNumSpots(i))
+    {
+        newButton.textContent = assignOperator(i);
+    }
+}
+
+//Determines if the current div is where
+function nonNumSpots(num)
+{
+    if(num === 3 || num === 7 || (num >= 11 && num < 15) || num === 15)
+    {
+        return true;
+    }
+    return false;
+}
+function assignOperator(num)
+{
+    switch(num)
+    {
+        case 3:
+            return "+"
+        case 7:
+            return "-";
+        case 11:
+            return "*";
+        case 12:
+            return "clear";
+        case 13:
+            return "0";
+        case 14:
+            return ".";
+        case 15:
+            return "÷";
+    }
+}
