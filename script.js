81828e933a3b5ad36c2c193c808253e4183fc11c
Eena <106263125+jrmullen2@users.noreply.github.com>
2023-03-02 16:08:50 -0800

    Create showNumber function and add event listener to buttons that contain a number 1 through 9

--- script.js
+++ script.js
@@ -33,49 +33,74 @@ function operate(op, num1, num2)
             break;
     }
 }
+let counter = 1;
 const container = document.querySelector("#container");
-for(let i = 0; i < 19; i++)
+for(let i = 1; i < 17; i++)
 {
     const newDiv = document.createElement("div");
     const newButton = document.createElement("button");
     newDiv.appendChild(newButton);
+    newDiv.classList.add("calcButton");
     container.appendChild(newDiv);
-    if(!(nonNumSpots(i)) && i < 11)
+    if(!(nonNumSpots(i)) && counter < 10)
     {
-        newButton.textContent = i.toString();
+        newButton.textContent = counter.toString();
+        newButton.classList.add("numButton");
+        counter += 1;
     }
     else if(nonNumSpots(i))
     {
         newButton.textContent = assignOperator(i);
+        newButton.classList.add("otherButton");
     }
 }
 
-//Determines if the current div is where
+//Determines if the current button should display a number 1-9 or not
 function nonNumSpots(num)
 {
-    if(num === 3 || num === 7 || (num >= 11 && num < 15) || num === 15)
+    if(num === 4 || num === 8 || (num >= 12 && num < 16) || num === 16)
     {
         return true;
     }
     return false;
 }
+//Provides the value that should be shown on a remaining button with no text content
 function assignOperator(num)
 {
     switch(num)
     {
-        case 3:
+        case 4:
             return "+"
-        case 7:
+        case 8:
             return "-";
-        case 11:
-            return "*";
         case 12:
-            return "clear";
+            return "*";
         case 13:
-            return "0";
+            return "clear";
         case 14:
             return ".";
         case 15:
+            return "0";
+        case 16:
             return "÷";
     }
+}
+let storedNum = 0;
+const numButtons = document.querySelectorAll(".numButton");
+numButtons.forEach((nButton) => {
+    nButton.addEventListener("click", () => {
+        showNumber(nButton.textContent)
+    });
+});
+let screen = document.querySelector("#numberDisplay");
+function showNumber(num)
+{
+    if(screen.textContent.length < 9)
+    {
+        let currentContent = screen.textContent;
+        currentContent += num;
+        screen.textContent = currentContent;
+    }
+    return;
+   
 }
